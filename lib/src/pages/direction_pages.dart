200e8c7b1d6c8c33230bd740a575ef62037451b5
Bratth05 <brataguilar25@gmail.com>
2023-11-04 09:05:33 -0600

    Agregar pages

--- lib/src/pages/direction_pages.dart
+++ lib/src/pages/direction_pages.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_navegacion/src/pages/direction_details_page.dart';
 
 class DirectionPage extends StatelessWidget {
   const DirectionPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Text("Informacion de direcciones"),
-      Icon(Icons.directions_transit)
-    ]));
+    return Center(
+      child: ElevatedButton(
+        child: const Text('Open detail'),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => const DirectionDetailPage()),
+          );
+        },
+      ),
+    );
   }
 }
